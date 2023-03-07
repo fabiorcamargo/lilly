56f46b6f8aeb6936dfdb117a3a57b4a9fe62b966
Fabio Camargo <fabiorcamargo@gmail.com>
2023-03-06 21:26:04 -0300

    up

--- resources/views/pages/app/portifolio/grid.blade.php
+++ resources/views/pages/app/portifolio/grid.blade.php
@@ -87,7 +87,7 @@
         @foreach ($portifolios as $portifolio)
         <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6 mb-4">
             <a href="./app-blog-post.html" class="card style-2 mb-md-0 mb-4">
-                <img src="{{asset("/bg/$portifolio->title/$portifolio->bg")}}" class="card-img-top" alt="...">
+                <img src="{{asset("$portifolio->bg")}}" class="card-img-top" alt="...">
                 <div class="card-body px-0 pb-0">
                     <h5 class="card-title mb-3">{{ $portifolio->title }}</h5>
                     <div class="media mt-4 mb-0 pt-1">
