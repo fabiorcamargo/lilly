23f0754904c13f43c0a95df5790a6d85eab47267
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-07 19:23:46 -0300

    up

--- resources/views/components/layout-footer.blade.php
+++ resources/views/components/layout-footer.blade.php
@@ -13,7 +13,7 @@
                 <div class="footer-section f-section-1">
                     <p target="_blank" href="https://lillyalmeida.com.br">© Llly Almeida - Fotografia</p>
                     {{--<p class="">Feito com <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></p>--}}
-                    <p  class="mt-2"> <a target="_blank" href="https://api.whatsapp.com/send?phone={{env('PHONE')}}&text=%F0%9F%93%B8Lilly%20Almeida%20Fotografia.%0AEstou%20entrando%20em%20contato%20via%20o%20site%20e%20gostaria%20de%20....."><x-widgets._w-svg class="text-success" svg="brand-whatsapp"/> (42) 9 9162-2889</a></p>
+                    <p  class="mt-2"> <a target="_blank" href="https://api.whatsapp.com/send?phone={{env('PHONE')}}&text=%F0%9F%93%B8Lilly%20Almeida%20Fotografia.%0AEstou%20entrando%20em%20contato%20via%20o%20site%20e%20gostaria%20de%20....."><x-widgets._w-svg class="text-success" svg="brand-whatsapp"/> {{env('PHONE')}}</a></p>
                     
                 </div>
                  
