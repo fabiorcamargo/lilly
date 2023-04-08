634522f2f1fcaf6af6dd3b6d197463ed037803b3
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-08 14:27:06 -0300

    up

--- app/Http/Controllers/Portfolio.php
+++ app/Http/Controllers/Portfolio.php
@@ -308,7 +308,7 @@ class Portfolio extends Controller
                     $cropHeight = $thumbnailImage->height();
                     
                     //$thumbnailImage->resize($cropWidth/4,$cropHeight/4);
-                    $thumbnailImage->encode('png', 50)->save($path."thumb".$name);
+                    $thumbnailImage->save($path."thumb".$name, 25);
 
                     //dd($photo->file);
                     //sleep(1);
