879802e540d67de98a68f17c557c0148fb20fbb7
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-10 21:54:20 -0300

    up

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -37,7 +37,7 @@
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer',env('GOOGLE_TAG_MANAGER_ID'));</script>
+    })(window,document,'script','dataLayer',{!env('GOOGLE_TAG_MANAGER_ID')!});</script>
     <!-- End Google Tag Manager -->
 
    <!-- Meta Pixel Code -->
@@ -99,7 +99,7 @@
     ]) @if ($scrollspy == 1) {{ $scrollspyConfig }} @else {{''}} @endif   @if (Request::routeIs('fullWidth')) layout="full-width"  @endif >
 
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=" . env('GOOGLE_TAG_MANAGER_ID')
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={!env('GOOGLE_TAG_MANAGER_ID')!}"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
