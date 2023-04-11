734364529f0facdcde1692ffddfddf64ff1eb46d
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-11 17:28:50 -0300

    up sitemap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,14 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://lillyalmeida.com.br/</loc>
+        <lastmod>{{ $post->created_at->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
     @foreach ($posts as $post)
         <url>
-            <loc>{{ url('/') }}/ml/app/portifolio/show/{{ $post->id }}</loc>
+            <loc>https://lillyalmeida.com.br/ml/app/portifolio/show/{{ $post->id }}</loc>
             <lastmod>{{ $post->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.8</priority>
