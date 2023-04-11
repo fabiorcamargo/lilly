dfa30a2c96c0859deae931a30a2f7dd892a26243
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-11 16:37:01 -0300

    redirect

--- resources/views/pages/app/portifolio/show.blade.php
+++ resources/views/pages/app/portifolio/show.blade.php
@@ -146,7 +146,7 @@
                     </div>
 
                     <div class="post-dynamic-meta mt-5 mb-5">
-
+Flist
                         <button class="btn btn-secondary me-4 mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                             <span class="btn-text-inner">1.1k</span>
