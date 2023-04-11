debf716fb58067680052101b59cfcc47518782ec
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-11 17:33:54 -0300

    up sitemap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>https://lillyalmeida.com.br/</loc>
-        <lastmod>{{ $post->created_at->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod>2023-03-12T03:42:54+00:00</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>
