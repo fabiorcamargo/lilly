cc6d138b81f33e784ac8192a02a868a5c38b88a5
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-10 22:16:38 -0300

    up

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -19,6 +19,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer',{{env('GOOGLE_TAG_MANAGER_ID')}});
+    </script>
+    <!-- End Google Tag Manager -->
+
     <title>{{ $pageTitle }}</title>
     <link rel="icon" type="image/x-icon" href="{{Vite::asset('resources/images/Logo-Lilly-Photo.png')}}"/>
     @vite(['resources/scss/layouts/vertical-light-menu/light/loader.scss'])
@@ -32,13 +43,7 @@
     @else @vite(['resources/layouts/vertical-dark-menu/loader.js'])
     @endif
 
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer',{{env('GOOGLE_TAG_MANAGER_ID')}});</script>
-    <!-- End Google Tag Manager -->
+    
 
    <!-- Meta Pixel Code -->
 <!--script>
@@ -98,10 +103,7 @@
         'maintanence' => (Request::routeIs('maintenance') ? true : false),
     ]) @if ($scrollspy == 1) {{ $scrollspyConfig }} @else {{''}} @endif   @if (Request::routeIs('fullWidth')) layout="full-width"  @endif >
 
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{env('GOOGLE_TAG_MANAGER_ID')}}"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+    
 
     <!-- BEGIN LOADER -->
     <x-layout-loader/>
@@ -188,6 +190,11 @@
                     </div>
                 @endif
 
+
+        <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{env('GOOGLE_TAG_MANAGER_ID')}}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
                 <!--  BEGIN FOOTER  -->
                 <x-layout-footer/>
                 <!--  END FOOTER  -->
